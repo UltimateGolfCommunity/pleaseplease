0fd0f91669aa13fb2530f5ffa759a118fc226d07
UltimateGolfCommunity <valuemyclubs@gmail.com>
2025-09-01 20:38:59 -0500

    🎬 Update homepage video background to use 0901.mov

--- app/page.tsx
+++ app/page.tsx
@@ -150,7 +150,7 @@ export default function HomePage() {
             playsInline
             className="w-full h-full object-cover opacity-40"
           >
-            <source src="/Firefly Have the two golfers exchanging contact information  636854.mp4" type="video/mp4" />
+            <source src="/0901.mov" type="video/quicktime" />
           </video>
           {/* Dark overlay */}
           <div className="absolute inset-0 bg-black/60"></div>
